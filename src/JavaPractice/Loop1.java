e6a36e27ea708dfa5772f09e1ae6b331d429e137
adhikarimadhu1 <madhuadhikari21@gmail.com>
2024-03-23 08:16:50 -0400

    new project creat

---
+++ src/JavaPractice/Loop1.java
@@ -0,0 +1,12 @@
+package JavaPractice;
+
+public class Loop1 {
+	public static void main(String[]args) {
+		
+		int cellNum[] = {10, 53, 68, 24, 19, 39};
+		for(int l = 0; l < cellNum.length ; l++) {
+			System.out.println(cellNum[l]);
+		}
+	}
+
+}
